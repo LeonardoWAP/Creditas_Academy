78b8a49f1a455e4744e846a3378a51f348c8fa1d
LeonardoWAP <leonardowillersap@gmail.com>
2022-03-22 15:33:55 -0300

    subindo

--- exercicios_JS/Palindromo.js
+++ exercicios_JS/Palindromo.js
@@ -1,10 +1,11 @@
 function palindromo(palindromo){
     var resposta = palindromo.toUpperCase().replace(/[^a-zA-Z]/g,"");
   
-    var reverseStr = resposta.toUpperCase().split('').reverse().join(''); 
+    var reverso = resposta.toUpperCase().split('').reverse().join(''); 
+    //transformar em um array , separa a palavra [a, m , o , r , a , r , o , m ,a ] reverse inverte a ordem do array e join transforma array em string.
     
     
-    if (reverseStr === resposta){
+    if (reverso === resposta){
         console.log("é um palindromo");
     }else{
             console.log("Não é um palindromo");
