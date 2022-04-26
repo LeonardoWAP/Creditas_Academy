67e5bee835e19ec2a147fdeea78a028d2e7b0fb0
LeonardoWAP <leonardowillersap@gmail.com>
2022-04-26 16:28:56 -0300

    subindo calculadora

---
+++ Calculadora/JS/calculadora.js
@@ -0,0 +1,29 @@
+let arr = document.getElementsByTagName("button")
+var visor;
+
+//sempre vai ser chamado essa função quando clicar no botão
+function pegarValor(value){
+  
+  showScreen(value)
+}
+
+function showScreen(value){
+  numero.innerHTML = resultado
+
+
+
+  // let elem = PEGAROQTATELA + value
+  console.log(numero)
+}
+
+// for (let i = 0 ; i < arr.length ;i++){
+//         arr[i].addEventListener("click", function myFunction() {
+//             console.log(arr[i].innerHTML)
+//           });
+// }
+
+// for (let i = 0 ; i < arr.length ;i++){
+//         arr[i].addEventListener("click", function myFunction() {
+//             console.log(arr[i].innerHTML)
+//           });
+// }
