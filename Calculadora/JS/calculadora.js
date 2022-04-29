6d3ca4383ce9de28eae08935582464b49a7fef59
LeonardoWAP <leonardowillersap@gmail.com>
2022-04-29 10:33:01 -0300

    calculadora Working refactor

--- Calculadora/JS/calculadora.js
+++ Calculadora/JS/calculadora.js
@@ -1,8 +1,8 @@
-let arr = document.getElementsByTagName("button")
 let visor = document.getElementById("resultado");
 var firstNumber;
 var secondNumber;
-var flag = 0 
+var operator;
+var visorIsResult = false;
 
 //sempre vai ser chamado essa função quando clicar no botão
 
@@ -21,78 +21,147 @@ function captureOperator(value){
 
 function captureNumber(value){
   // console.log(value)
+  if (visorIsResult){
+    visorIsResult = false;
+    visor.innerHTML = ""
+  }
   showScreen(value)
 }
 
 
 function sum(firstNumber,secondNumber){
-  
   return firstNumber + secondNumber
-  
 }
+
+function subtrai(firstNumber,secondNumber){
+  return firstNumber - secondNumber
+}
+
+function multiply(firstNumber,secondNumber){
+  return firstNumber * secondNumber
+}
+
+function division(firstNumber,secondNumber){
+  return firstNumber / secondNumber
+}
+
 
 function convertStringToNumber(number){
-  return parseInt(number)
+  if (!number){
+    number = 0;
+  }
+  return parseFloat(number)
 }
 
-function verifyTypeNumber(){
-  if (flag == 0 ){
-    firstNumber = visor.innerHTML
-    flag = 1
-    console.log("capturando o fisrtnumber = " + firstNumber)
- 
-    visor.innerHTML = ""
+
+function realizaOperacao(tipoDeOperador){
+  let result
+  switch(tipoDeOperador){
+
+    case "+":
+      result = sum(firstNumber,secondNumber)
+      console.log("result  " + result)
+      showScreen(result)
+      visorIsResult = true;
+      firstNumber = result
+      secondNumber = null
+      return
+
+    case "-":
+      result = subtrai(firstNumber,secondNumber)
+      console.log("result  " + result)
+      showScreen(result)
+      visorIsResult = true;
+      firstNumber = result
+      secondNumber = null
+      return
+
+    case "*":
+      result = multiply(firstNumber,secondNumber)
+      console.log("result  " + result)
+      showScreen(result)
+      visorIsResult = true;
+      firstNumber = result
+      secondNumber = null
+      return
+
+      case "/":
+        result = division(firstNumber,secondNumber)
+        console.log("result  " + result)
+        showScreen(result)
+        visorIsResult = true;
+        firstNumber = result
+        secondNumber = null
+        return
+
+    default:
+      console.log("outra operação")
   }
+}
 
+function verifyTypeNumber(tipoDeOperador){
 
- else if(flag == 1){
-      
+  if (!firstNumber){
+    firstNumber = visor.innerHTML
+    console.log("capturando o fisrtnumber = " + firstNumber)
+    visor.innerHTML = ""
+    
+  }
+  else {
     secondNumber = visor.innerHTML
     console.log("capturando o secund = " + secondNumber)
-    flag = 0
     visor.innerHTML = ""
     
     console.log("fazer a soma do  " + firstNumber + " + " + secondNumber)
-    let firstNumberInt = convertStringToNumber(firstNumber)
-    let secondNumberInt = convertStringToNumber(secondNumber)
+    firstNumber = convertStringToNumber(firstNumber)
+    secondNumber = convertStringToNumber(secondNumber)
 
+    realizaOperacao(tipoDeOperador)
 
+ }
 
-    let result = sum(firstNumberInt,secondNumberInt)
-
-    console.log("result  " + result)
-
-    showScreen(result)
-  }
 }
 
 function captureValueOperator(value){
-  if (value == "c"){
-    visor.innerHTML = ""
-  }
   
+  if (value == "="){
+    verifyTypeNumber(operator)
+    visorIsResult = true;
+  }
+
   if (value == "+"){
-    verifyTypeNumber()
+    operator = "+"
+    verifyTypeNumber(operator)
   }
 
   if (value == "-"){
+    operator = "-"
+    verifyTypeNumber(operator)
     console.log("esse é o -")
   }
 
   if (value == "/"){
+    operator = "/"
+    verifyTypeNumber(operator)
     console.log("esse é o /")
   }
 
-
   if (value == "*"){
-    
+    operator = "*"
+    verifyTypeNumber(operator)
     console.log("esse é o *")
-
   }
 
-
-  if (value == "="){
-    verifyTypeNumber()
+  if (value == "."){
+    verifyTypeNumber(operator)
+    console.log("esse é o .")
   }
 
+  if (value == "c"){
+    visor.innerHTML = ""
+    firstNumber= null;
+    secondNumber= null;
+    operator = null;
+    visorIsResult = false;
+  }
 }
